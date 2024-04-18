67447d7c07fba2a41f4593be315453a61dbff04e
rushi3691 <rushi3691@outlook.com>
2024-04-19 01:47:38 +0530

    Refactor registration process to handle edits and improve validation in validator.ts

--- api/auth/validator.ts
+++ api/auth/validator.ts
@@ -1,26 +1,37 @@
 import { z } from "zod";
 
-export const RegisterUserSchema = z.object({
-  name: z.string(),
-  gender: z.enum(["MALE", "FEMALE", "OTHER"]),
-  dob: z.coerce.date(),
-  role: z.literal("ALUMNI"),
-  institute_number: z.string(),
-  joining_year: z.number(),
-  graduation_year: z.number(),
-  program: z.enum(["BTECH", "MTECH", "PHD"]),
-  branch: z.string(),
-  hostel_room_no: z.string(),
-  mobile: z.string(),
-  linkedin: z.string().optional(),
-  twitter: z.string().optional(),
-  github: z.string().optional(),
-  work_experience: z.string(),
-  skills: z.array(z.string()),
-  address: z.string(),
+const RegisterSchema = z.object({
   is_consent: z.boolean(),
-  reg_or_edit: z.enum(["REGISTER", "EDIT"]),
+  reg_or_edit: z.literal("REGISTER"),
 });
+
+const EditSchema = z.object({
+  reg_or_edit: z.literal("EDIT"),
+});
+
+const RegisterEditSchema = z.union([RegisterSchema, EditSchema]);
+
+export const RegisterUserSchema = z
+  .object({
+    name: z.string(),
+    gender: z.enum(["MALE", "FEMALE", "OTHER"]),
+    dob: z.coerce.date(),
+    role: z.literal("ALUMNI"),
+    institute_number: z.string(),
+    joining_year: z.number(),
+    graduation_year: z.number(),
+    program: z.enum(["BTECH", "MTECH", "PHD"]),
+    branch: z.string(),
+    hostel_room_no: z.string(),
+    mobile: z.string(),
+    linkedin: z.string().optional(),
+    twitter: z.string().optional(),
+    github: z.string().optional(),
+    work_experience: z.string(),
+    skills: z.array(z.string()),
+    address: z.string(),
+  })
+  .and(RegisterEditSchema);
 
 export type RegisterUserSchemaType = z.infer<typeof RegisterUserSchema>;
 
